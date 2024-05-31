34d2c33cf8589f3fb5d2ac16e68ef3f3e2ba0bad
Felipe Mendes <you@example.com>
2024-05-31 16:01:29 -0300

    exercircio finalizado

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string palavraCensurada = "!@#$%#@";
+string fraseDigitada, fraseCensurada;
+
+Console.Write("Digite uma frase: ");
+fraseDigitada = Console.ReadLine()!;
+
+fraseCensurada = fraseDigitada
+ .Replace("bobalhao", palavraCensurada)
+ .Replace("otario", palavraCensurada)
+ .Replace("bobo", palavraCensurada)
+ .Replace("vacilao", palavraCensurada)
+ .Replace("feio", palavraCensurada)
+ .Replace("burro", palavraCensurada)
+ .Replace("panaca", palavraCensurada);
+
+ Console.WriteLine(fraseCensurada);
